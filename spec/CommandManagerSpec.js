21a0982458523efd9f0c3a627ae7ae9493f9ff36
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-06-22 22:53:45 +0200

    added jasmine BDD framework

---
+++ spec/CommandManagerSpec.js
@@ -0,0 +1,8 @@
+describe("CommandManager", function() {
+
+  it("initializes properly", function() {
+    var c = new CommandManager();
+    expect(c).toBeDefined();
+  });
+
+});
---
+++ spec/SpecHelper.js
@@ -0,0 +1,10 @@
+// Sample matcher extension
+//
+// beforeEach(function() {
+//   this.addMatchers({
+//     toDoSomethingCool: function(coolParameter) {
+//       var currentObject= this.actual;
+//       return currentObject.coolParameter === coolParameter;
+//     }
+//   })
+// });
