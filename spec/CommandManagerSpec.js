e4f777056bc1c9f936cc283f1c75223359a2158c
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-06-23 11:06:32 +0200

    CommandManager initial spec

--- spec/CommandManagerSpec.js
+++ spec/CommandManagerSpec.js
@@ -1,8 +1,46 @@
 describe("CommandManager", function() {
+  var commandManager;
+  beforeEach(function(){
+    commandManager = new CommandManager();
+  });
 
-  it("initializes properly", function() {
-    var c = new CommandManager();
-    expect(c).toBeDefined();
+  describe("constructor", function() {
+    it('is defined', function(){
+      expect(commandManager).toBeDefined();
+    });
+    it('has no commands', function(){
+      expect(commandManager.commands.length).toBe(0);
+    });
+  });
+
+  describe('#addCommand()', function(){
+    var helloWorld;
+    beforeEach(function(){
+      helloWorld = new Command('helloWorld', function(){
+        console.log("Hello world from test command called 'helloWorld'!");
+      });
+    });
+
+    it('returns true on successful add', function(){
+      expect(commandManager.addCommand(helloWorld)).toBe(true);
+    });
+    it('remembers new commands', function(){
+      commandManager.addCommand(helloWorld);
+      expect(commandManager.commands.length).toBe(1);
+    });
+  });
+
+  describe ('#getCommand()', function(){
+    var helloWorld;
+    beforeEach(function(){
+      helloWorld = new Command('helloWorld', function(){
+        console.log("Hello world from test command called 'helloWorld'!");
+      });
+      commandManager.addCommand(helloWorld);
+    });
+    it('returns exactly the same thing', function(){
+      expect(commandManager.getCommand(helloWorld)===helloWorld).toBeTruthy();
+    });
   });
 
 });
